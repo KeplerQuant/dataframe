7d5c9df6bf4ed826ff450a9f6293c3b346e1d7d6
Jay <106968807+ethelbell@users.noreply.github.com>
2024-06-16 20:54:23 +0800

    delete indicator

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,6 @@
 
 /// This module contains the `DataFrame` structure and associated functions.
 mod frame;
-/// This module contains technical analysis functions.
-mod indicator;
 /// This module contains the `Row` structure and associated functions.
 mod row;
 /// This module contains the `Series` structure and associated functions.
@@ -20,4 +18,4 @@ mod series;
 pub use frame::DataFrame;
 pub use row::Row;
 pub use series::Series;
-pub use yata::prelude::OHLCV;
+pub use yata;
